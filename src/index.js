978e08998ca4ed54f80f9d96141010e8719a452f
VietDung922003 <dungviet2k3@gmail.com>
2025-03-29 08:40:17 +0700

    add connect to Db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const { default: mongoose } = require("mongoose");
 dotenv.config()
 
 const app = express()
@@ -7,6 +8,14 @@ const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
     return res.send('Hello, world! Bonjour');
+})
+
+mongoose.connect(`mongodb+srv://21521942:${process.env.MONGO_DB}@master.uqz2k.mongodb.net/?retryWrites=true&w=majority&appName=Master`)
+.then(() => {
+    console.log('Connect to Db success!')
+})
+.catch((err) => {
+    console.log(err)
 })
 
 app.listen(port, () => {
