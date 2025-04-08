3827074799b14f0a09bc253acbf08501ed3450b0
1609Dzuaa <datrua3152003@gmail.com>
2025-04-08 16:11:54 +0700

    fix: error codes and cors allowed

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,174 +1,179 @@
-const UserService = require('../services/UserService')
-const JwtService = require('../services/JwtService')
+const UserService = require("../services/UserService");
+const JwtService = require("../services/JwtService");
 
 const createUser = async (req, res) => {
-    try{
-        const { email, password, confirmPassword } = req.body
-        var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-        const isCheckEmail = reg.test(email)
-        if (!email || !password || !confirmPassword) {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is required'
-            })
-        }else if (!isCheckEmail) 
-        {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The input is email'
-            })
-        }else if (password !== confirmPassword)
-        {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'Confirm password must match the password'
-            })
-        }
-        const response = await UserService.createUser(req.body)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+  try {
+    const { email, password, confirmPassword } = req.body;
+    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isCheckEmail = reg.test(email);
+    if (!email || !password || !confirmPassword) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "The input is required",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "The input is email",
+      });
+    } else if (password !== confirmPassword) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "Confirm password must match the password",
+      });
     }
-}
+    const response = await UserService.createUser(req.body);
 
-const loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        if (!email || !password) {
-            return res.status(400).json({
-                status: 'ERR',
-                message: 'Email and password are required'
-            });
-        }
-
-        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-        if (!reg.test(email)) {
-            return res.status(400).json({
-                status: 'ERR',
-                message: 'Invalid email format'
-            });
-        }
-
-        const response = await UserService.loginUser({ email, password });
-        return res.status(200).json(response);
-    } catch (e) {
-        return res.status(500).json({
-            status: 'ERR',
-            message: e.message || 'Login err',
-        });
+    if (response.status === "ERR") {
+      return res.status(400).json(response);
     }
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "Email and password are required",
+      });
+    }
+
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if (!reg.test(email)) {
+      return res.status(400).json({
+        status: "ERR",
+        message: "Invalid email format",
+      });
+    }
+
+    const response = await UserService.loginUser({ email, password });
+
+    if (response.status === "ERR") {
+      return res.status(401).json(response);
+    }
+
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(500).json({
+      status: "ERR",
+      message: e.message || "Login err",
+    });
+  }
+};
 
 const updateUser = async (req, res) => {
-    try{
-        const userId = req.params.id
-        const data = req.body
-        //console.log('data', data)
+  try {
+    const userId = req.params.id;
+    const data = req.body;
+    //console.log('data', data)
 
-        if(!userId)
-        {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'User ID is required'
-            })
-        }
-        //console.log('id', userId)
-        const response = await UserService.updateUser(userId, data)
-        return res.status(200).json(response)
-    }catch(e){
-        console.log('error')
-        return res.status(404).json({
-            message: "Error occured",
-            error: e.message
-        })
+    if (!userId) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "User ID is required",
+      });
     }
-}
+    //console.log('id', userId)
+    const response = await UserService.updateUser(userId, data);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log("error");
+    return res.status(404).json({
+      message: "Error occured",
+      error: e.message,
+    });
+  }
+};
 
 const deleteUser = async (req, res) => {
-    try{
-        const userId = req.params.id
+  try {
+    const userId = req.params.id;
 
-        if(!userId)
-        {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'User ID is required'
-            })
-        }
-        console.log('id', userId)
-        const response = await UserService.deleteUser(userId)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+    if (!userId) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "User ID is required",
+      });
     }
-}
+    console.log("id", userId);
+    const response = await UserService.deleteUser(userId);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getAllUser = async (req, res) => {
-    try{
-        const { limitUser, page } = req.query
-        const response = await UserService.getAllUser(Number(limitUser) || 8, Number(page) || 0)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
+  try {
+    const { limitUser, page } = req.query;
+    const response = await UserService.getAllUser(
+      Number(limitUser) || 8,
+      Number(page) || 0
+    );
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const getDetailUser = async (req, res) => {
-    try{
-        const userId = req.params.id
+  try {
+    const userId = req.params.id;
 
-        if(!userId)
-        {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'User ID is required'
-            })
-        }
-        console.log('id', userId)
-        const response = await UserService.getDetailUser(userId)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e
-        })
+    if (!userId) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "User ID is required",
+      });
     }
-}
+    console.log("id", userId);
+    const response = await UserService.getDetailUser(userId);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e,
+    });
+  }
+};
 
 const refreshToken = async (req, res) => {
-    try{
-        const token = req.headers.token.split(" ")[1]
+  try {
+    const token = req.headers.token.split(" ")[1];
 
-        if(!token)
-        {
-            return res.status(200).json({
-                status: 'ERR',
-                message: 'The token is required'
-            })
-        }
-        //console.log('id', userId)
-        const response = await JwtService.refreshTokenJwtService(token)
-        return res.status(200).json(response)
-    }catch(e){
-        return res.status(404).json({
-            message: e.message
-        })
+    if (!token) {
+      return res.status(200).json({
+        status: "ERR",
+        message: "The token is required",
+      });
     }
-}
+    //console.log('id', userId)
+    const response = await JwtService.refreshTokenJwtService(token);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(404).json({
+      message: e.message,
+    });
+  }
+};
 
 module.exports = {
-    createUser,
-    loginUser,
-    updateUser,
-    deleteUser,
-    getAllUser,
-    getDetailUser,
-    refreshToken
-}
+  createUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+  getAllUser,
+  getDetailUser,
+  refreshToken,
+};
